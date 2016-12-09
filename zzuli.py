8ac3fb92ddfcc07dc6652da0dcc8757f8ff28839
zeliges <jianaosiding@users.noreply.github.com>
2016-12-09 13:14:07 +0800

    Update zzuli.py

--- zzuli.py
+++ zzuli.py
@@ -5,7 +5,6 @@ import urllib.request
 loginurl = 'http://my.zzuli.edu.cn/userPasswordValidate.portal'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-    'Cookie': 'Hm_lvt_1e7bb0586b7e61911a64e086492a7916=1477624531; Hm_lpvt_1e7bb0586b7e61911a64e086492a7916=1477624531; _ga=GA1.3.297408136.1477624531; _gat=1; JSESSIONID=00009jL4YpBHimeCREM4FNAcem5:164h0btfq',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'zh-CN,zh;q=0.8'
